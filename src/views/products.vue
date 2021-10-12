c2549cfffda0212cea6f5564a9acbbb14a34f519
“hoyi” <keira.yen0715@gmail.com>
2021-10-12 15:05:37 +0800

    add sortProducts feature

--- src/views/products.vue
+++ src/views/products.vue
@@ -22,10 +22,39 @@
                 <thead>
                     <tr>
                         <th>活動名稱</th>
-                        <th>分類</th>
-                        <th>開始日期</th>
-                        <th>結束日期</th>
-                        <th>狀態</th>
+                        <th>
+                            分類
+                            <div class="sortProducts">
+                                <input v-model="sortProducts" value="sortByType" type="radio" id="sortByType" name="sortProducts">
+                                <label for="sortByType">
+                                    <span class="material-icons-outlined">keyboard_arrow_down</span>
+                                </label>
+                            </div>
+                        </th>
+                        <th>開始日期
+                            <div class="sortProducts">
+                                <input v-model="sortProducts" type="radio" value="sortByStartDate" id="sortByStartDate" name="sortProducts">
+                                <label for="sortByStartDate">
+                                    <span class="material-icons-outlined">keyboard_arrow_down</span>
+                                </label>
+                            </div>
+                        </th>
+                        <th>結束日期
+                            <div class="sortProducts">
+                                <input v-model="sortProducts" type="radio" value="sortByEndDate" id="sortByEndDate" name="sortProducts">
+                                <label for="sortByEndDate">
+                                    <span class="material-icons-outlined">keyboard_arrow_down</span>
+                                </label>
+                            </div>
+                        </th>
+                        <th>狀態
+                            <div class="sortProducts">
+                                 <input v-model="sortProducts" type="radio" value="sortByStatus" id="sortByStatus" name="sortProducts">
+                                <label for="sortByStatus">
+                                    <span class="material-icons-outlined">keyboard_arrow_down</span>
+                                </label>
+                            </div>
+                        </th>
                         <th>操作</th>
                     </tr>
                 </thead>
@@ -49,7 +78,7 @@ import ProductModal from '@/components/ProductModal.vue'
 import ExitCheck from '@/components/ExitCheck.vue'
 import ProductTable from '../components/ProductTable.vue'
 import OnLoad from '../components/OnLoad.vue'
-import {computed,onBeforeMount} from 'vue'
+import {computed,onBeforeMount,ref,watch} from 'vue'
 import {useStore} from 'vuex'
 
 export default {
@@ -74,16 +103,43 @@ export default {
         //拿取後端資料
         onBeforeMount(()=>{
             store.dispatch('getProduct')
+            
         })
         const checkProductLoaded = computed(()=>store.getters.productLoaded)
         const productData = computed(()=>store.getters.productData)
 
+        //排序
+        const sortProducts = ref(null)
+
+        watch(sortProducts,()=>{
+            //console.log(sortProducts.value)
+            switch(sortProducts.value){
+                case 'sortByType': productData.value.sort((a,b)=>{
+                    return a.eventCategory > b.eventCategory ? 1:-1
+                });
+                break;
+                case 'sortByStartDate': productData.value.sort((a,b)=>{
+                    return a.eventStartDate > b.eventStartDate ? 1:-1
+                });
+                break;
+                case 'sortByEndDate': productData.value.sort((a,b)=>{
+                    return a.eventEndDate > b.eventEndDate ? 1:-1
+                });
+                break;
+                case 'sortByStatus': productData.value.sort((a,b)=>{
+                    return a.eventActive > b.eventActive ? 1:-1
+                }) 
+            }
+        })
+        
         return{
             showExitCheckModal,
             showModal,
             showingModal,
             checkProductLoaded,
-            productData
+            productData,
+            sortProducts
+
         }
     },
     
@@ -91,3 +147,14 @@ export default {
     
 }
 </script>
+
+<style lang="scss" scoped>
+    .sortProducts{
+        position: relative;
+        
+        input{
+            position: absolute;
+            display: none;
+        }
+    }
+</style>
